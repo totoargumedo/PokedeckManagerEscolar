4c4feaa351b1584e22d9f84c809be03be241bf84
totoargumedo <totoargumedo@gmail.com>
2025-10-28 11:14:10 -0300

    Corrección imagenes

--- pokemons.js
+++ pokemons.js
@@ -4,27 +4,27 @@ const pokemons = [
     tipo: "Eléctrico",
     vida: 100,
     imagen:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+      "https://static.wikia.nocookie.net/espokemon/images/7/77/Pikachu.png/revision/latest/scale-to-width-down/1000?cb=20150621181250",
   },
   {
     nombre: "Charmander",
     tipo: "Fuego",
     vida: 100,
     imagen:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+      "https://static.wikia.nocookie.net/espokemon/images/5/56/Charmander.png/revision/latest?cb=20221210013209",
   },
   {
     nombre: "Squirtle",
     tipo: "Agua",
     vida: 100,
     imagen:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+      "https://static.wikia.nocookie.net/espokemon/images/e/e3/Squirtle.png/revision/latest?cb=20160309230820",
   },
   {
     nombre: "Bulbasaur",
     tipo: "Planta",
     vida: 100,
     imagen:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+      "https://static.wikia.nocookie.net/espokemon/images/4/43/Bulbasaur.png/revision/latest/scale-to-width-down/1000?cb=20170120032346",
   },
 ];
